fa44f9e53a171cc7dfa2276b4d11695f36244a56
jl917 <julong1988@naver.com>
2024-05-18 00:11:36 +0900

    feat: start sitemap

---
+++ src/index.ts
@@ -0,0 +1,104 @@
+import type { RspressPlugin } from "@rspress/shared";
+import { statSync, writeFileSync } from "fs";
+
+type ChangeFreq =
+  | "always"
+  | "hourly"
+  | "daily"
+  | "weekly"
+  | "monthly"
+  | "yearly"
+  | "never";
+
+type Priority =
+  | "0.0"
+  | "0.1"
+  | "0.2"
+  | "0.3"
+  | "0.4"
+  | "0.5"
+  | "0.6"
+  | "0.7"
+  | "0.8"
+  | "0.9"
+  | "1.0";
+
+interface Sitemap {
+  loc: string;
+  lastmod?: string;
+  changefreq?: ChangeFreq;
+  priority?: Priority;
+}
+
+interface CustomMaps {
+  [prop: string]: Sitemap;
+}
+
+interface Options {
+  domain?: string;
+  customMaps?: CustomMaps;
+  defaultPriority?: Priority;
+  defaultChangeFreq?: ChangeFreq;
+}
+
+const generateNode = (sitemap: Sitemap): string => {
+  let result = "<url>";
+  for (const [tag, value] of Object.entries(sitemap)) {
+    result += `<${tag}>${value}</${tag}>`;
+  }
+  result += "</url>";
+  return result;
+};
+
+const generateXml = (sitemaps: Sitemap[]) => {
+  console.log(sitemaps.length);
+  return `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${sitemaps.reduce(
+    (node, sitemap) => node + generateNode(sitemap),
+    ""
+  )}</urlset>`;
+};
+
+export default function rspressPluginSitemap(options: Options): RspressPlugin {
+  options = {
+    domain: "https://rspress.dev",
+    customMaps: {},
+    defaultChangeFreq: "monthly",
+    defaultPriority: "0.5",
+    ...options,
+  };
+  let sitemaps: Sitemap[] = [];
+  let set = new Set();
+  return {
+    name: "rspress-plugin-sitemap",
+    config(config) {
+      return config;
+    },
+    extendPageData(pageData, isProd) {
+      if (isProd) {
+        if (!set.has(pageData.id)) {
+          set.add(pageData.id);
+          sitemaps.push({
+            loc: `${options.domain}${pageData.routePath}`,
+            lastmod: statSync(pageData._filepath).mtime.toISOString(),
+            priority:
+              pageData.routePath === "/" ? "1.0" : options.defaultPriority,
+            changefreq: options.defaultChangeFreq,
+            ...((options.customMaps &&
+              options.customMaps[pageData.routePath]) ||
+              {}),
+          });
+        }
+      }
+    },
+    afterBuild(config, isProd) {
+      if (isProd) {
+        writeFileSync(
+          `./${
+            config.builderConfig?.output?.distPath?.root || "doc_build"
+          }/sitemap.xml`,
+          generateXml(sitemaps)
+        );
+      }
+    },
+  };
+}
